6ec7acd5aae0632c05c868316389aa8d8715882f
Muhammed Berisso <muhammedb8826@gmail.com>
2025-10-26 21:33:50 +0300

    Update next config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -7,13 +7,9 @@ const nextConfig: NextConfig = {
     unoptimized: true, // Required for static export
   },
   eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors. Only enable if needed.
     ignoreDuringBuilds: true,
   },
   typescript: {
-    // Warning: Dangerously allow production builds to successfully complete even if
-    // your project has type errors. Only enable if needed.
     ignoreBuildErrors: true,
   },
 };
