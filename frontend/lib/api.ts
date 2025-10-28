ba360284ebc4e2936213b99a0781deabc03f73cf
ararso1 <arasoalisho2@gmail.com>
2025-10-28 16:41:39 +0300

    update

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:4000';
-// const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'https://wanofi-api.daminaa.org';
+// const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:4000';
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'https://wanofi-api.daminaa.org';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
